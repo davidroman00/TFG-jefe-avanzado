e83e8aaebd2e27a9467ba2308a56330f55867398
davidroman00 <davidro3421@gmail.com>
2024-11-16 17:36:52 +0100

    correcciones en las referencias

--- Assets/Boss/Scripts/Utility Scripts/BossReferences.cs
+++ Assets/Boss/Scripts/Utility Scripts/BossReferences.cs
@@ -5,17 +5,25 @@ public class BossReferences : MonoBehaviour
     //Here, there are stored and exported every object required for the boss to operate correctly.
     //The fact that there are some values at 'none' on inspector means they are not necessary for the current phase of the boss.    
     [SerializeField]
-    Transform[] _fanRangedSpawnPoint;
+    Transform[] _fanRangedSpawnPoints;
     [SerializeField]
     Transform[] _fanBossPositions;
     [SerializeField]
-    Transform[] _sweepRangedSpawnPoint;
+    Transform[] _sweepRangedSpawnPoints;
     [SerializeField]
     Transform _sweepBossPosition;
     [SerializeField]
-    Transform[] _crossRangedSpawnPoint;
+    Transform[] _crossRangedSpawnPoints;
     [SerializeField]
     Transform _crossBossPosition;
+    [SerializeField]
+    Transform _meleeAttackSpawnPoint;
+    [SerializeField]
+    GameObject _sweepRangedProjectile;
+    [SerializeField]
+    GameObject _fanAndCrossRangedProjectile;
+    [SerializeField]
+    GameObject _meleeAttackDevice;
     [SerializeField]
     Transform _ultimateBossPosition;
     [SerializeField]
@@ -29,12 +37,16 @@ public class BossReferences : MonoBehaviour
     [SerializeField]
     Transform _playerTransform;
 
-    public Transform[] FanRangedSpawnPoint { get { return _fanRangedSpawnPoint; } }
+    public Transform[] FanRangedSpawnPoints { get { return _fanRangedSpawnPoints; } }
     public Transform[] FanBossPositions { get { return _fanBossPositions; } }
-    public Transform[] SweepRangedSpawnPoint { get { return _sweepRangedSpawnPoint; } }
+    public Transform[] SweepRangedSpawnPoints { get { return _sweepRangedSpawnPoints; } }
     public Transform SweepBossPosition { get { return _sweepBossPosition; } }
-    public Transform[] CrossRangedSpawnPoint { get { return _crossRangedSpawnPoint; } }
+    public Transform[] CrossRangedSpawnPoints { get { return _crossRangedSpawnPoints; } }
     public Transform CrossBossPosition { get { return _crossBossPosition; } }
+    public Transform MeleeAttackSpawnPoint { get { return _meleeAttackSpawnPoint; } }
+    public GameObject SweepRangedProjectile { get { return _sweepRangedProjectile; } }
+    public GameObject FanAndCrossRangedProjectile { get { return _fanAndCrossRangedProjectile; } }
+    public GameObject MeleeAttackDevice { get { return _meleeAttackDevice; } }
     public Transform UltimateBossPosition { get { return _ultimateBossPosition; } }
     public Transform UltimateWeaponSpawnPoint { get { return _ultimateWeaponSpawnPoint; } }
     public Transform UltimateDeviceSpawnPoint { get { return _ultimateDeviceSpawnPoint; } }
