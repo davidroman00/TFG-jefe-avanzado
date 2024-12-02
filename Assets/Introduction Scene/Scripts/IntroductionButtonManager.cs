c768e617358db1ca07af50f1fe90d548a6a040aa
davidroman00 <davidro3421@gmail.com>
2024-12-02 17:49:08 +0100

    acabo la escena introductoria + pongo musica placeholder a la escena

--- Assets/Introduction Scene/Scripts/IntroductionButtonManager.cs
+++ Assets/Introduction Scene/Scripts/IntroductionButtonManager.cs
@@ -1,18 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroductionButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static string Username;
+    [SerializeField]
+    GameObject _holaText;
+    [SerializeField]
+    GameObject _bienvenidaText;
+    [SerializeField]
+    GameObject _flamanteText;
+    [SerializeField]
+    GameObject _legendarioText;
+    [SerializeField]
+    GameObject _nombreText;
+    [SerializeField]
+    TMP_InputField _usernameInputField;
+    [SerializeField]
+    Button _confirmUsernameButton;
+    int _currentAmountOfClicks;
+    [SerializeField]
+    float _waitTime;
+    bool _isWaiting;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        _currentAmountOfClicks = 0;
+    }
     void Update()
     {
-        
+        switch (_currentAmountOfClicks)
+        {
+            case 0:
+                _holaText.SetActive(true);
+                break;
+            case 1:
+                _holaText.SetActive(false);
+                _bienvenidaText.SetActive(true);
+                break;
+            case 2:
+                _bienvenidaText.SetActive(false);
+                _flamanteText.SetActive(true);
+                break;
+            case 3:
+                _flamanteText.SetActive(false);
+                _legendarioText.SetActive(true);
+                break;
+            case 4:
+                _legendarioText.SetActive(false);
+                _nombreText.SetActive(true);
+                break;
+            case 5:
+                _usernameInputField.gameObject.SetActive(true);
+                Debug.Log(_usernameInputField.text);
+                if (_usernameInputField.text != "")
+                {
+                    _confirmUsernameButton.gameObject.SetActive(true);
+                }
+                else
+                {
+                    _confirmUsernameButton.gameObject.SetActive(false);
+                }
+                break;
+        }
+        HandleClickAmount();
+    }
+    void HandleClickAmount()
+    {
+        if (Input.anyKey && !_isWaiting && _currentAmountOfClicks < 5)
+        {
+            StartCoroutine(HandleWaitTime());
+        }
+    }
+    IEnumerator HandleWaitTime()
+    {
+        _currentAmountOfClicks++;
+        _isWaiting = true;
+        yield return new WaitForSeconds(_waitTime);
+        _isWaiting = false;
+    }
+    public void ConfirmUsername()
+    {
+        Username = _usernameInputField.text;
+        SceneManager.LoadScene(1);
     }
 }
