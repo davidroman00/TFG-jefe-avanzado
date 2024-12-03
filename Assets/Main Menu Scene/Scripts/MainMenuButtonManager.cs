e374adc08c7af7b57fe444f8fb1cdf3db0437437
davidroman00 <davidro3421@gmail.com>
2024-12-03 15:44:13 +0100

    main menu terminado (falta el video de fondo)

--- Assets/Main Menu Scene/Scripts/MainMenuButtonManager.cs
+++ Assets/Main Menu Scene/Scripts/MainMenuButtonManager.cs
@@ -1,12 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject _mainMenu;
+    [SerializeField]
+    GameObject _settingsMenu;
     public void PlayButton()
     {
         SceneManager.LoadScene(2);
+    }
+    public void SettingsButton()
+    {
+        _mainMenu.SetActive(false);
+        _settingsMenu.SetActive(true);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
